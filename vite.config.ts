867c582a8a81cc1f32a5045167592f99d77aa427
yunsuper <yunsuper80@gmaile.com>
2025-11-14 18:50:50 +0900

    Apply .gitignore

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
-
-
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), vanillaExtractPlugin()],
-});
